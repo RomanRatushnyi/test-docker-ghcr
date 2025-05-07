8dd0f6bbeab3976cc378b121fc6f6266dbc2b9e2
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2025-05-07 11:35:18 +0300

    Update script

--- deploy-check.sh
+++ deploy-check.sh
@@ -6,24 +6,13 @@ REPO_OWNER=romanratushnyi
 REPO_NAME=test-docker-ghcr
 IMAGE_NAME=ghcr.io/$REPO_OWNER/$REPO_NAME
 CONTAINER_NAME=test-docker-ghcr
+CURRENT_IMAGE=""
 CURRENT_VERSION=""
 
 echo $GH_TOKEN | docker login ghcr.io -u $REPO_OWNER --password-stdin
 
-if docker ps -a --format '{{.Names}}' | grep -q "^$CONTAINER_NAME$"; then
-  echo "Trying to get version from running container $CONTAINER_NAME..."
-  CURRENT_VERSION=$(docker exec "$CONTAINER_NAME" node -p "require('./package.json').version" 2>/dev/null)
-fi
-
-if [ -z "$CURRENT_VERSION" ]; then
-  echo "Container not running or version not detected. Trying to extract from image..."
-  
-  docker create --name "tmp-version-check-container" "$IMAGE_NAME:latest" > /dev/null
-
-  CURRENT_VERSION=$(docker cp "tmp-version-check-container":/app/package.json - | tar -Ox package.json | jq -r .version 2>/dev/null)
-
-  docker rm "$TEMP_CONTAINER_NAME" > /dev/null
-fi
+CURRENT_IMAGE=$(docker inspect --format '{{.Config.Image}}' "$CONTAINER_NAME" 2>/dev/null)
+CURRENT_VERSION=$(echo "$CURRENT_IMAGE" | grep -o 'v[0-9]\+\.[0-9]\+\.[0-9]\+')
 
 echo "Current version: ${CURRENT_VERSION:-<unknown>}"
 
@@ -35,7 +24,7 @@ if [ "$CURRENT_VERSION" != "$LATEST_VERSION" ]; then
   
   docker compose pull
 
-  docker tag "$IMAGE_NAME:$LATEST_VERSION" "$IMAGE_NAME:latest"
+  docker tag "$IMAGE_NAME:latest" "$IMAGE_NAME:$LATEST_VERSION"
   
   docker compose down
   
